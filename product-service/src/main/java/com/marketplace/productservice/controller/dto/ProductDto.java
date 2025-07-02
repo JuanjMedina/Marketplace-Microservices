8df6923196738ba93ff3abfb5fe832e1d8fbe064
Juan Medina <jmedinaguerrero847@gmail.com>
2025-07-02 16:00:27 -0500

    Refactor ProductDto to include necessary fields for product representation

--- product-service/src/main/java/com/marketplace/productservice/controller/dto/ProductDto.java
+++ product-service/src/main/java/com/marketplace/productservice/controller/dto/ProductDto.java
@@ -1,4 +1,15 @@
 package com.marketplace.productservice.controller.dto;
 
-public record ProductDto() {
+public record ProductDto(String name,
+                         String description,
+                         Double price,
+                         Integer quantity) {
+
+//    public ProductDto(String name, String description, String imageUrl, String sellerId, Double price, Integer quantity) {
+//        this.name = name;
+//        this.description = description;
+//        this.sellerId = sellerId;
+//        this.price = price;
+//        this.quantity = quantity;
+//    }
 }
